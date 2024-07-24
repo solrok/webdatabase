19f634a2e172c6dd7e6c41d440658eab88c433d9
solrok <upendrapratapsingh899@gmail.com>
2024-07-24 13:21:34 +0000

    updating css

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return render_template(home.html)
+    return render_template('home.html')
 print(__name__)
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
